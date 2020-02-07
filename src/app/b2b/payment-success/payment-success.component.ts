42301ee62b79bbef28ba455bda8e9979ff3fd953
sanjeevreddy <sanjeevreddy644@gmail.com>
2020-02-07 17:56:53 +0530

    changes

--- src/app/b2b/payment-success/payment-success.component.ts
+++ src/app/b2b/payment-success/payment-success.component.ts
@@ -326,11 +326,13 @@ export class PaymentSuccessComponent implements OnInit {
       console.log("groundForm data: ===>", JSON.stringify(groundForm));
       this.teejanServices.getGroundServiceReservation(groundForm).subscribe((data: any) => {
         this.spinner.hide();
+        localStorage.setItem('groundBookingResponse', JSON.stringify(data.body));
+
         if (data.bookingStatus == "Confirmed") {
           localStorage.setItem("BookingStatus"  , "true")  
 
-          localStorage.setItem('groundBookingResponse', JSON.stringify(data));
-          
+          localStorage.setItem('groundBookingResponse', JSON.stringify(data.body));
+
           localStorage.removeItem("groundCart");
 
           this.router.navigateByUrl('b2c/mybooking')
